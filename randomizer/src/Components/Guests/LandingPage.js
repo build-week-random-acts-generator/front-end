037837c0d0a6d98a95071aec1f3955bb6c5bc5a4
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-06-06 18:28:01 -0400

    remove material ui / added bootstrap / began login page

--- randomizer/src/Components/Guests/LandingPage.js
+++ randomizer/src/Components/Guests/LandingPage.js
@@ -6,7 +6,7 @@ class LandingPage extends Component {
             <div className = "container">
                 <div className = "jumbotron mt-5">
                     <div className = "col-sm-8 mx-auto">
-                        <h1 className = "text-center">Welcome</h1>
+                        <h1 className = "text-center">Randomizer</h1>
                     </div>
                 </div>
             </div>
