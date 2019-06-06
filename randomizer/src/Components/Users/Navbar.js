347f93fcfe8cdc90452cce9eee01fd5e98b5d270
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-06-06 17:59:43 -0400

    nav

--- randomizer/src/Components/Users/Navbar.js
+++ randomizer/src/Components/Users/Navbar.js
@@ -21,7 +21,64 @@ class Navbar extends Component {
                         Register
                     </Link>
                 </li>
+                <li className = "nav-item">
+                    <a href = "" onClick = {this.logOut.bind(this)} className = "nav-link">
+                        Logout
+                    </a>
+                </li>
+
             </ul>
+        )
+        const userLinks = (
+            <ul className ="navbar-nav">
+                <li className = "nav-item">
+                    <Link to = "/home" className = "nav-link">
+                        Home
+                    </Link>
+                </li>
+                <li className = "nav-item">
+                    <Link to = "/contacts" className = "nav-link">
+                        Contacts
+                    </Link>
+                </li>
+                <li className = "nav-item">
+                    <Link to = "/services" className = "nav-link">
+                        Services
+                    </Link>
+                </li>
+                <li className = "nav-item">
+                    <a href = "" onClick = {this.logOut.bind(this)} className = "nav-link" >
+                        Logout
+                    </a>
+                </li>
+            </ul>
+        )
+
+        return (
+            <nav className = "navbar navbar-expand-lg navbar-dark bg-dark rounded">
+                <button className = "navbar-toggler"
+                    type = "button"
+                    data-toggle = "collapse"
+                    data-target = "#navbar1"
+                    aria-controls = "navbar1"
+                    aria-label = "Toggle navigation">
+                        <span className = "navbar-toggle-icon"></span>
+                    </button>
+
+                    <div className = "collapse navbar-collapse justify-content md-center"
+                    id = "navbar1">
+                        <ul className = "navbar-nav">
+                            <li className = "nav-item">
+                                <Link to ="/" className = "nav-link">
+                                    Home
+                                </Link>
+                            </li>
+                        </ul>
+                        {localStorage.token ? userLinks : loginRegLink}
+                    </div>
+            </nav>
         )
     }
 }
+
+export default withRouter(Navbar);
